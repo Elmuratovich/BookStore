01ffed444e9c88cf915d1f0ac734a9b1dca47760
Makhmud <makhmudkuzikaev@gmail.com>
2023-11-09 20:29:17 +0600

    Security

--- aspnet-core/src/Acme.BookStore.Application.Contracts/Permissions/BookStorePermissionDefinitionProvider.cs
+++ aspnet-core/src/Acme.BookStore.Application.Contracts/Permissions/BookStorePermissionDefinitionProvider.cs
@@ -8,9 +8,12 @@ public class BookStorePermissionDefinitionProvider : PermissionDefinitionProvide
 {
     public override void Define(IPermissionDefinitionContext context)
     {
-        var myGroup = context.AddGroup(BookStorePermissions.GroupName);
-        //Define your own permissions here. Example:
-        //myGroup.AddPermission(BookStorePermissions.MyPermission1, L("Permission:MyPermission1"));
+        var bookStoreGroup = context.AddGroup(BookStorePermissions.GroupName, L("Permission:BookStore"));
+
+        var bookPermission = bookStoreGroup.AddPermission(BookStorePermissions.Books.Default, L("Permission:Books"));
+        bookPermission.AddChild(BookStorePermissions.Books.Create, L("Permission:Books.Create"));
+        bookPermission.AddChild(BookStorePermissions.Books.Edit, L("Permission:Books.Edit"));
+        bookPermission.AddChild(BookStorePermissions.Books.Delete, L("Permission:Books.Delete"));
     }
 
     private static LocalizableString L(string name)
